d69eb396010c58516bd75a3d5eab6090da59398a
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-04-17 21:26:16 +0530

    Add mock data for events and trending topics

---
+++ src/mock-data/mock-data.js
@@ -0,0 +1,57 @@
+const trendingTopics = [
+    {
+        "id": 1,
+        "name": "Sports",
+        "identifier": "SPORTS"
+    },
+    {
+        "id": 2,
+        "name": "Artificial Intelligence",
+        "identifier": "AI"
+    },
+    {
+        "id": 3,
+        "name": "DevOps",
+        "identifier": "DEVOPS"
+    },
+    {
+        "id": 4,
+        "name": "Version Controls",
+        "identifier": "VERSION_CONTROL"
+    },
+    {
+        "id": 5,
+        "name": "Amazon Web Services",
+        "identifier": "AWS"
+    },
+],
+
+const events = [
+    {
+        "id": 1,
+        "name": "Google HashCode",
+        "description": "Online coding competition for developer",
+        "redirectLink": "https://www.google.com",
+        "date": "2022-04-30",
+        "regClosingDate": "2022-04-21",
+        "organizer": "Google Inc",
+    },
+    {
+        "id": 2,
+        "name": "AWS Meetup",
+        "description": "Amazon Web Service - Machine Learning with AWS",
+        "redirectLink": "https://www.aws.com",
+        "date": "2022-05-30",
+        "regClosingDate": "2022-04-21",
+        "organizer": "Amazon",
+    },
+    {
+        "id": 1,
+        "name": "Hacktoberfest 2022",
+        "description": "World largest open source program for developer all over the world",
+        "redirectLink": "https://www.hacktoberfest.com",
+        "date": "2022-11-30",
+        "regClosingDate": "2022-10-21",
+        "organizer": "Intel, Github, AppWrite",
+    },
+]
