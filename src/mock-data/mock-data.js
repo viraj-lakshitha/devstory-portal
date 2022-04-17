3fe4ee486130087738e758b848c33b939623026c
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-04-17 21:37:18 +0530

    Change mock data to javascript objects and add sample data for articles

--- src/mock-data/mock-data.js
+++ src/mock-data/mock-data.js
@@ -1,57 +1,89 @@
 const trendingTopics = [
     {
-        "id": 1,
-        "name": "Sports",
-        "identifier": "SPORTS"
+        id: 1,
+        name: "Sports",
+        identifier: "SPORTS"
     },
     {
-        "id": 2,
-        "name": "Artificial Intelligence",
-        "identifier": "AI"
+        id: 2,
+        name: "Artificial Intelligence",
+        identifier: "AI"
     },
     {
-        "id": 3,
-        "name": "DevOps",
-        "identifier": "DEVOPS"
+        id: 3,
+        name: "DevOps",
+        identifier: "DEVOPS"
     },
     {
-        "id": 4,
-        "name": "Version Controls",
-        "identifier": "VERSION_CONTROL"
+        id: 4,
+        name: "Version Controls",
+        identifier: "VERSION_CONTROL"
     },
     {
-        "id": 5,
-        "name": "Amazon Web Services",
-        "identifier": "AWS"
+        id: 5,
+        name: "Amazon Web Services",
+        identifier: "AWS"
     },
 ],
 
 const events = [
     {
-        "id": 1,
-        "name": "Google HashCode",
-        "description": "Online coding competition for developer",
-        "redirectLink": "https://www.google.com",
-        "date": "2022-04-30",
-        "regClosingDate": "2022-04-21",
-        "organizer": "Google Inc",
+        id: 1,
+        name: "Google HashCode",
+        description: "Online coding competition for developer",
+        redirectLink: "https://www.google.com",
+        createdAt: "2022-04-30",
+        regClosingDate: "2022-04-21",
+        organizer: "Google Inc",
     },
     {
-        "id": 2,
-        "name": "AWS Meetup",
-        "description": "Amazon Web Service - Machine Learning with AWS",
-        "redirectLink": "https://www.aws.com",
-        "date": "2022-05-30",
-        "regClosingDate": "2022-04-21",
-        "organizer": "Amazon",
+        id: 2,
+        name: "AWS Meetup",
+        description: "Amazon Web Service - Machine Learning with AWS",
+        redirectLink: "https://www.aws.com",
+        createdAt: "2022-05-30",
+        regClosingDate: "2022-04-21",
+        organizer: "Amazon",
     },
     {
-        "id": 1,
-        "name": "Hacktoberfest 2022",
-        "description": "World largest open source program for developer all over the world",
-        "redirectLink": "https://www.hacktoberfest.com",
-        "date": "2022-11-30",
-        "regClosingDate": "2022-10-21",
-        "organizer": "Intel, Github, AppWrite",
+        id: 1,
+        name: "Hacktoberfest 2022",
+        description: "World largest open source program for developer all over the world",
+        redirectLink: "https://www.hacktoberfest.com",
+        createdAt: "2022-11-30",
+        regClosingDate: "2022-10-21",
+        organizer: "Intel, Github, AppWrite",
     },
+]
+
+const userSpecificArticles = [
+    {
+        id: article-1-2343721,
+        title: "Welcome to the MLOPS",
+        displayDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+        createdBy: "2343721",
+        createdAt: "1650211367",
+        updatedAt: "1650211367",
+        interactions: {
+            up: 12,
+            down: 3,
+            comments: [
+                {
+                    id: article-1-2343721-comment-1,
+                    description: "Awesome Stuff!!!",
+                    createdBy: "24875322",
+                    createdAt: "1650211367",
+                    updatedBy: "1650211367",
+                },
+                {
+                    id: article-1-2343721-comment-1,
+                    description: "Keep it up!!!",
+                    createdBy: "23431322",
+                    createdAt: "1650211367",
+                    updatedBy: "1650211367",
+                }
+            ]
+        },
+    }
 ]
